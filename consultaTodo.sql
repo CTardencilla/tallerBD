4f49905c004d3ada0b89e2b2c58f43b842df1d39
CTardencilla <calebtardencilla@gmail.com>
2026-04-30 21:13:06 -0600

    67

---
+++ consultaTodo.sql
@@ -0,0 +1,20 @@
+USE Recibo;
+GO
+
+-- Ver todos los clientes
+SELECT * FROM dbo.Cliente;
+
+-- Ver todas las cuentas
+SELECT * FROM Cuenta;
+
+-- Ver todos los comercios
+SELECT * FROM Comercio;
+
+-- Ver todas las transacciones
+SELECT * FROM Transaccion;
+
+-- Ver todos los detalles
+SELECT * FROM DetalleTransaccio;
+
+-- Ver todas las terminales
+SELECT * FROM Terminal;
